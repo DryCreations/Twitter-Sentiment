d5bb88d0b50a6241ab95c5b45849cfa508fc3a01
Hayden Mankin <DryCreations@gmail.com>
2020-11-13 23:24:52 -0500

    render frontend

--- app.py
+++ app.py
@@ -4,15 +4,12 @@ from flask_cors import CORS
 import requests
 
 app = Flask(__name__,
-            static_folder = "./dist/static",
+            static_folder = "./dist",
             template_folder = "./dist")
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    if app.debug:
-        return requests.get(f'http://localhost:8080/{path}').text
-    return '<h1>Nick is ...</h1>'
-    # return render_template("index.html")
+    # return requests.get('http://localhost:8080/{}'.format(path)).text
+    return render_template("index.html")
