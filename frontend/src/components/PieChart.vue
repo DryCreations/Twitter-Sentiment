f8766372261cc53f88ca001206efa79fa6dbdd0d
Ninjacat698 <48922027+Ninjacat698@users.noreply.github.com>
2020-11-14 18:59:07 -0500

    updated Chart and created PieChart

---
+++ frontend/src/components/PieChart.vue
@@ -0,0 +1,11 @@
+<script>
+import { Pie } from 'vue-chartjs'
+export default {
+  props: ['chartData', 'options'],
+  extends: Pie,
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+
+</script>
--- frontend/src/views/Chart.vue
+++ frontend/src/views/Chart.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="Chart">
     <TopBar title="Chart"/>
+    <PieChart :chartData="chartData" :options="options"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import TopBar from '@/components/TopBar.vue'
+import PieChart from '@/components/PieChart.vue'
 
 export default {
+  data: () => ({
+    chartData: {
+      datasets: [{
+        data: [10, 20, 30],
+        backgroundColor: ['#f87979', '#45f542', '#707070']
+      }],
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: [
+        'Negative',
+        'Postive',
+        'Unsure'
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
   name: 'Chart',
   components: {
-    TopBar
+    TopBar,
+    PieChart
   }
 }
 </script>
