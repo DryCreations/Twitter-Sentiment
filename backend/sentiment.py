ccb80023f0e77ab421acf6727bcd0a2148ac2403
Hayden Mankin <DryCreations@gmail.com>
2020-11-14 22:15:26 -0500

    get actual sentiment

--- backend/sentiment.py
+++ backend/sentiment.py
@@ -4,6 +4,7 @@ from numpy import argmax
 from typing import Tuple
 from keras.models import model_from_json
 from keras.preprocessing.sequence import pad_sequences
+import json
 
 
 class Sentiment:
@@ -28,7 +29,7 @@ class Sentiment:
         batch_size = 64
         if len(tweets) < batch_size: batch_size = len(tweets)
 
-        tweet_bodies = [t.full_text for t in tweets]
+        tweet_bodies = [t['full_text'] for t in tweets]
         tokenized_tweets = self.tokenizer.texts_to_sequences(tweet_bodies)
         tokenized_tweets = pad_sequences(tokenized_tweets, maxlen=39, dtype='int32', value=0)
         results = self.model.predict(tokenized_tweets, batch_size=batch_size, verbose=2)
@@ -36,4 +37,4 @@ class Sentiment:
         confidence = [s[argmax(s)] for s in results]
         for i in range(len(tweets)):
             tweets[i]['sentiment'] = sentiment[i]
-            tweets[i]['confidence'] = confidence[i]
+            tweets[i]['confidence'] = json.dumps(str(confidence[i]))
