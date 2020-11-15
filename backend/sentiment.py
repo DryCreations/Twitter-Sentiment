8fda6b9caaa83f760684648f662d09cc7820f8ed
Ghostjs123 <1011collin@gmail.com>
2020-11-14 22:06:22 -0500

    Updated to mutate list

--- backend/sentiment.py
+++ backend/sentiment.py
@@ -24,13 +24,16 @@ class Sentiment:
             self.tokenizer = pickle.load(handle)
 
 
-    def get_sentiment(self, tweets: list) -> list:
+    def get_sentiment(self, tweets: list):
         batch_size = 64
         if len(tweets) < batch_size: batch_size = len(tweets)
 
-        tokenized_tweets = self.tokenizer.texts_to_sequences(tweets)
+        tweet_bodies = [t.full_text for t in tweets]
+        tokenized_tweets = self.tokenizer.texts_to_sequences(tweet_bodies)
         tokenized_tweets = pad_sequences(tokenized_tweets, maxlen=39, dtype='int32', value=0)
         results = self.model.predict(tokenized_tweets, batch_size=batch_size, verbose=2)
-        sentiments = ['positive' if argmax(s) == 1 else 'negative' for s in results]
-        scores = [s[argmax(s)] for s in results]
-        return list(zip(tweets, sentiments, scores))
+        sentiment = ['positive' if argmax(s) == 1 else 'negative' for s in results]
+        confidence = [s[argmax(s)] for s in results]
+        for i in range(len(tweets)):
+            tweets[i]['sentiment'] = sentiment[i]
+            tweets[i]['confidence'] = confidence[i]
